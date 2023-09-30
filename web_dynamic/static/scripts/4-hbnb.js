8374a178b3332fb450936fc4f03632d6d03200e5
Korchea <6334@holbertonstudents.com>
2023-09-30 09:59:14 -0700

    5 need to work when id of button is clicked

--- web_dynamic/static/scripts/4-hbnb.js
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -1,80 +1,64 @@
 $(document).ready(function () {
-  // Initialize an empty array to store Amenity IDs
-  let selectedAmenities = [];
+  let selectAmenities = {};
 
-  // Listen for changes on all checkboxes with class "checkbox"
-  $(".checkbox").on("change", function () {
-    let id = $(this).data('id');
-    let name = $(this).data('name');
+  $('input[type="checkbox"]').change(function () {
+    let amenityID = $(this).data('id');
+    let amenityName = $(this).data('name');
 
-    if ($(this).is(":checked")) {
-      // Checkbox is checked, add Amenity ID to the array
-      selectedAmenities.push(name);
+    if ($(this).is(':checked')) {
+      selectAmenities[amenityID] = amenityName;
     } else {
-      // Checkbox is unchecked, remove Amenity ID from the array
-      const index = selectedAmenities.indexOf(name);
-      if (index !== -1) {
-        selectedAmenities.splice(index, 1);
+      delete selectAmenities[amenityID];
+    }
+
+    let amenities = Object.values(selectAmenities).join(', ');
+    if (amenities.length > 30) {
+      amenities = amenities.substring(0, 30) + "...";
+    }
+    $('.amenities h4').text(amenities);
+  });
+  $.ajax({
+    method: 'GET',
+    url: 'http://localhost:5001/api/v1/status/',
+    success: function (data) {
+      if (data.status === 'OK') {
+        $('div#api_status').addClass('available');
+      }
+      else {
+        $('div#api_status').removeClass();
       }
     }
-
-    // Update the content of the <h4> element with the selected amenities
-    $('.amenities h4').html(selectedAmenities.join(", "));
   });
 });
-
-$(document).ready(function () {
-  $.get("http://0.0.0.0:5001/api/v1/status/", function (data, status, xhr) {
-    if (xhr.status >= 200 && xhr.status <= 299) {
-      // API is available, add the "available" class
-      $("div#api_status").addClass("available");
-    } else {
-      // API is not available, remove the "available" class
-      $("div#api_status").removeClass("available");
-    }
-  });
-});
-
-fetch('http://0.0.0.0:5001/api/v1/places_search', {
+fetch('http://localhost:5001/api/v1/places_search', {
   method: 'POST',
+  body: JSON.stringify({}),
   headers: {
-    'Content-Type': 'application/json',
-  },
-  body: JSON.stringify({ data: {} }),
+    'Content-Type': 'application/json'
+  }
 })
-  .then((response) => response.json())
-  .then((data) => {
-    data.forEach((result) => {
-      places_data.innerHTM = `
-      <article>
-        <div class="title_box">
-          <h2>${result.name}</h2>
-          <div class="price_by_night">${result.price_by_night}</div>
-        </div>
-        <div class="information">
-          <div class="max_guest">${result.max_guest}</div>
-          <div class="number_rooms">${result.number_rooms} Bedrooms</div>
-          <div class="number_bathrooms">${result.number_bathrooms} Bathrooms</div>
-        </div>
-        <div class="description">
-          ${result.description}
-              </div>
-      </article>`;
+  .then(response => response.json())
+  .then(data => {
+    const placesCont = document.querySelector('section.places');
+    data.forEach(place => {
+      const article = document.createElement('article');
+      article.innerHTML = `
+    <div class="title_box">
+    <h2>${place.name}</h2>
+    <div class="price_by_night">${place.price_by_night}</div>
+  </div>
+  <div class="information">
+    <div class="max_guest">${place.max_guest}</div>
+    <div class="number_rooms">${place.number_rooms} Bedrooms</div>
+    <div class="number_bathrooms">${place.number_bathrooms} Bathrooms</div>
+  </div>
+  <div class="description">
+    ${place.description}
+  </div>
+  `;
+      placesCont.appendChild(article);
     });
-  });
-
-fetch('http://localhost:5001/api/v1/status', {
-  method: 'GET',
-})
-  .then((response) => response.json())
-  .then((data) => {
-    if (data.status === 'OK') {
-      document.querySelector('div#api_status').classList.add('available');
-    }
   })
-  .catch((error) => {
-    console.error('Error', error);
-  });
 
 fetch('http://localhost:5001/api/v1/places_search', {
   method: 'POST',
